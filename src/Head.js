a507d86dc9ca7ab50a7e4f69082df603e2f7dace
JosephMartinez3005 <52721764+JosephMartinez3005@users.noreply.github.com>
2021-03-30 13:58:23 -0700

    Added H1 into head along with nasa png

---
+++ src/Head.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Head(){
+    return (
+        <>
+        <img src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2Fe%2Fe5%2FNASA_logo.svg%2F1200px-NASA_logo.svg.png&f=1&nofb=1' height='100rem'></img>
+        <h1>Welcome to the NASA space Genarator!</h1>
+        </>
+    )
+}
+export default Head
---
+++ src/SpacePhoto.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function SpacePhoto(){
+
+    return(
+        <>
+            {
+                
+            }
+        </>
+    )
+}
+
+export default SpacePhoto
