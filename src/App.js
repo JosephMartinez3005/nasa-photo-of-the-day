b2b0de4288669ab1dbc4f8314b1c550ff5418a75
JosephMartinez3005 <52721764+JosephMartinez3005@users.noreply.github.com>
2021-03-30 14:01:45 -0700

    Added axios to the space photo componet

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./App.css";
+import axios from 'axios'
 import Head from './Head'
+import SpacePhoto from './SpacePhoto'
+
 function App() {
   return (
     <div className="App">
--- src/SpacePhoto.js
+++ src/SpacePhoto.js
@@ -1,11 +1,15 @@
-import React from 'react'
-
+import React, {useEffect} from 'react'
+import axios from 'axios'
 function SpacePhoto(){
 
     return(
         <>
             {
-                
+                useEffect(()=>{
+                    axios.get('')
+                    .then()
+                    .catch()
+                },[])
             }
         </>
     )
